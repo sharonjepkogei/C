21e4cd88b99aa80a8c13284db83571b40780ac37
sharonjepkogei <158994677+sharonjepkogei@users.noreply.github.com>
2024-03-09 14:10:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# Initialize the 3D array with zeros (or any other value) using list comprehensions
+a = [[[0 for col in range(2)] for col in range(3)] for row in range(3)]
+
+# Print the array
+print(a)
